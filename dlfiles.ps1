1177f95e0b45ca9324bd465797616dd511430079
sanfordvdev <sanfordvdev@gmail.com>
2021-02-19 10:35:03 -0600

    Fixed variables for second download

--- dlfiles.ps1
+++ dlfiles.ps1
@@ -3,4 +3,4 @@ $destination = 'C:\Users\sanfordvdev\Downloads\'
 Start-BitsTransfer -Source $source -Destination $destination
 $source1 = 'https://github.com/microsoft/cascadia-code/archive/main.zip'
 $destination1 = 'C:\Users\sanfordvdev\Downloads\'
-Start-BitsTransfer -Source $source -Destination $destination
+Start-BitsTransfer -Source $source1 -Destination $destination1
