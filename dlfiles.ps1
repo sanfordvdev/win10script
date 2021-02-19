35fb96b9271b69eabdabcbb8961a48ef847665fd
sanfordvdev <sanfordvdev@gmail.com>
2021-02-19 10:39:42 -0600

    added cdda launcher to download files

--- dlfiles.ps1
+++ dlfiles.ps1
@@ -4,3 +4,6 @@ Start-BitsTransfer -Source $source -Destination $destination
 $source1 = 'https://github.com/microsoft/cascadia-code/archive/main.zip'
 $destination1 = 'C:\Users\sanfordvdev\Downloads\'
 Start-BitsTransfer -Source $source1 -Destination $destination1
+$source2 = 'https://github.com/remyroy/CDDA-Game-Launcher/archive/master.zip'
+$destination2 = 'C:\Users\sanfordvdev\Downloads\cddamaster.zip'
+Start-BitsTransfer -Source $source2 -Destination $destination2
