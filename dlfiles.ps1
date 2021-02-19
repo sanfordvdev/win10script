c4730a73f8c77788ccc1de92628b5d7e9219ee6d
sanfordvdev <sanfordvdev@gmail.com>
2021-02-19 10:33:42 -0600

    Added Cascadia Code to dlfiles

--- dlfiles.ps1
+++ dlfiles.ps1
@@ -1,3 +1,6 @@
 $source = 'https://github.com/SomeDeadGuy/UndeadPeopleTileset/archive/master.zip'
 $destination = 'C:\Users\sanfordvdev\Downloads\'
 Start-BitsTransfer -Source $source -Destination $destination
+$source1 = 'https://github.com/microsoft/cascadia-code/archive/main.zip'
+$destination1 = 'C:\Users\sanfordvdev\Downloads\'
+Start-BitsTransfer -Source $source -Destination $destination
