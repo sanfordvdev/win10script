1fbfd78d0f6b86d717ff8a45d7d98ee96c747a46
sanfordvdev <sanfordvdev@gmail.com>
2021-02-19 10:53:22 -0600

    Added a sleep timer in dlfiles.ps1

--- dlfiles.ps1
+++ dlfiles.ps1
@@ -1,9 +1,11 @@
 $source = 'https://github.com/SomeDeadGuy/UndeadPeopleTileset/archive/master.zip'
 $destination = 'C:\Users\sanfordvdev\Downloads\'
 Start-BitsTransfer -Source $source -Destination $destination
+Start-Sleep -Seconds 10
 $source1 = 'https://github.com/microsoft/cascadia-code/archive/main.zip'
 $destination1 = 'C:\Users\sanfordvdev\Downloads\'
 Start-BitsTransfer -Source $source1 -Destination $destination1
+Start-Sleep -Seconds 10
 $source2 = 'https://github.com/remyroy/CDDA-Game-Launcher/archive/master.zip'
 $destination2 = 'C:\Users\sanfordvdev\Downloads\cddamaster.zip'
 Start-BitsTransfer -Source $source2 -Destination $destination2
